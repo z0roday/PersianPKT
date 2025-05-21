6898bda68c26519baf8140e8dbb31780b4a3f488
z0roday <100026917+z0roday@users.noreply.github.com>
2025-05-21 12:53:13 -0700

    Add core module structure

---
+++ persianpkt/src/core/mod.rs
@@ -0,0 +1,11 @@
+mod package_manager;
+mod dependency_resolver;
+mod cache;
+mod mirror;
+mod security;
+
+pub use package_manager::*;
+pub use dependency_resolver::*;
+pub use cache::*;
+pub use mirror::*;
+pub use security::*; 
